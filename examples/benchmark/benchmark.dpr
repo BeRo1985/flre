a2af8bb28d6ba58b9b1346137003c9357dd841cc
Benjamin Rosseaux <benjamin@rosseaux.de>
2015-09-04 05:25:42 +0200

    - Updated benchmark source with the correct usage of RawByteString

--- examples/benchmark/benchmark.dpr
+++ examples/benchmark/benchmark.dpr
@@ -81,21 +81,21 @@ uses
 
 const BenchmarkCount=1;
 
-      BenchmarkPatterns:array[0..14] of ansistring=('Twain',
-                                                   '(?i)Twain',
-                                                   '[a-z]shing',
-                                                   'Huck[a-zA-Z]+|Saw[a-zA-Z]+',
-                                                   '\b\w+nn\b',
-                                                   '[a-q][^u-z]{13}x',
-                                                   'Tom|Sawyer|Huckleberry|Finn',
-                                                   '(?i)Tom|Sawyer|Huckleberry|Finn',
-                                                   '.{0,2}(Tom|Sawyer|Huckleberry|Finn)',
-                                                   '.{2,4}(Tom|Sawyer|Huckleberry|Finn)',
-                                                   'Tom.{10,25}river|river.{10,25}Tom',
-                                                   '[a-zA-Z]+ing',
-                                                   '\s[a-zA-Z]{0,12}ing\s',
-                                                   '([A-Za-z]awyer|[A-Za-z]inn)\s',
-                                                   '["''][^"'']{0,30}[?!\.]["'']');
+      BenchmarkPatterns:array[0..14] of TFLRERawByteString=('Twain',
+                                                            '(?i)Twain',
+                                                            '[a-z]shing',
+                                                            'Huck[a-zA-Z]+|Saw[a-zA-Z]+',
+                                                            '\b\w+nn\b',
+                                                            '[a-q][^u-z]{13}x',
+                                                            'Tom|Sawyer|Huckleberry|Finn',
+                                                            '(?i)Tom|Sawyer|Huckleberry|Finn',
+                                                            '.{0,2}(Tom|Sawyer|Huckleberry|Finn)',
+                                                            '.{2,4}(Tom|Sawyer|Huckleberry|Finn)',
+                                                            'Tom.{10,25}river|river.{10,25}Tom',
+                                                            '[a-zA-Z]+ing',
+                                                            '\s[a-zA-Z]{0,12}ing\s',
+                                                            '([A-Za-z]awyer|[A-Za-z]inn)\s',
+                                                            '["''][^"'']{0,30}[?!\.]["'']');
 
 {$ifdef windows}
 function IsDebuggerPresent:boolean; stdcall; external 'kernel32.dll' name 'IsDebuggerPresent';
